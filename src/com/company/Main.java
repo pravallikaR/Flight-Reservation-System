9a73ab37bdaf99a21ffad38003b73f8fd0b9bbe3
pravallika <pravallikatheitachi@gmail.com>
2021-04-23 18:06:52 +0530

    completed FRS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+
+        Ticket regularTicket = new RegularTicket("12345", "Himachal Pradesh",
+                "Madhya Pradesh", "DepartureDateTime", "ArrivalDateTime",
+                "12C", 8000, false, null, null, "lunch");
+
+        Ticket touristTicket = new TouristTicket("67890", "India", "Japan",
+                "DepartureDateTime", "ArrivalDateTIme",
+                "14F", 200000, true, null, null,
+                "Hayaku", new String[]{"Fujikima", "Ichiraaku"});
+
+        printTicketDetails(regularTicket);
+        printTicketDetails(touristTicket);
+    }
+
+    public static void printTicketDetails(Ticket ticket) {
+        System.out.println(ticket.getPnr());
     }
 }
